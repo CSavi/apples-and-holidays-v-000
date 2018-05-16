befcfcdf4e56b1e536c185ab58d3c337b419a4f6
Carrie Savinell <carrielane89@gmail.com>
2018-05-16 14:48:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,8 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_hash[:spring][:memorial_day][1] = "Grill"
+  holiday_hash[:spring][:memorial_day] << supply 
+end   
 
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
