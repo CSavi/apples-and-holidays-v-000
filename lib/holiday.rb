2d7ecab2600d780b9e0009b4cacee0ff8c68cac0
Carrie Savinell <carrielane89@gmail.com>
2018-05-16 14:44:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -33,7 +33,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
       if supplies == 
     
     [:spring][:memorial_day][1] = "Table Cloth"
-  binding.pry
+  end 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
