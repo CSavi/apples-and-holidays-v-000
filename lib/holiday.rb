6974090e6c1be4eaca734597b52e1a8f2bc12445
Carrie Savinell <carrielane89@gmail.com>
2018-05-16 16:05:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,6 +45,7 @@ def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, holiday|
    puts "#{season.to_s.capitalize!}:" 
    holiday.each do |holiday, supply| 
+     binding.pry
       puts "#{holiday.to_s.split(" ").capitalize.join(" ")}"
  
    end 
